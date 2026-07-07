c753be568a667a0947efafa5f09d1a86aefb1b41
unknown <Administrator@ROMERITO.AD>
2026-07-07 14:00:04 -0300

    Primeira rota

--- src/main/java/br/com/cadastrochamados/cadastrochamados/CadastroChamadosApplication.java
+++ src/main/java/br/com/cadastrochamados/cadastrochamados/CadastroChamadosApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CadastrochamadosApplication {
+public class CadastroChamadosApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(CadastrochamadosApplication.class, args);
+		SpringApplication.run(CadastroChamadosApplication.class, args);
 	}
 
 }
