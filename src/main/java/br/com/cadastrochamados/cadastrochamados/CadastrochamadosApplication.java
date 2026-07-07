2f350b31bacaad91407726c55d652422fca5cec9
unknown <Administrator@ROMERITO.AD>
2026-07-07 13:19:37 -0300

    first commit

---
+++ src/main/java/br/com/cadastrochamados/cadastrochamados/CadastrochamadosApplication.java
@@ -0,0 +1,13 @@
+package br.com.cadastrochamados.cadastrochamados;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadastrochamadosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadastrochamadosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/cadastrochamados/cadastrochamados/CadastrochamadosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.cadastrochamados.cadastrochamados;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadastrochamadosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
