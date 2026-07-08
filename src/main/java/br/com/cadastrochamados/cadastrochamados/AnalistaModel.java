6540612205cc12006b1d277c7f872a7b103f942a
Dan-dev1305 <danielgoncalves.paz@gmail.com>
2026-07-07 21:47:59 -0300

    Cria uma entidade no banco de dados a partir do AnalistaModel

---
+++ src/main/java/br/com/cadastrochamados/cadastrochamados/AnalistaModel.java
@@ -0,0 +1,50 @@
+package br.com.cadastrochamados.cadastrochamados;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "tb_cadastro")
+public class AnalistaModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
+    String nome;
+    String email;
+    String senioridade;
+
+    public AnalistaModel() {
+    }
+
+    public AnalistaModel(String nome, String email, String senioridade) {
+        this.nome = nome;
+        this.email = email;
+        this.senioridade = senioridade;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSenioridade() {
+        return senioridade;
+    }
+
+    public void setSenioridade(String senioridade) {
+        this.senioridade = senioridade;
+    }
+
+
+}
