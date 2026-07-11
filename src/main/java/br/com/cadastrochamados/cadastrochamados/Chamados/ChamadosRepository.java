a21b5c07cfd9ad670f64831fa35e6b769f931420
Dan-dev1305 <danielgoncalves.paz@gmail.com>
2026-07-11 16:10:24 -0300

    Criacao do repositoory jpa do chamado

---
+++ src/main/java/br/com/cadastrochamados/cadastrochamados/Chamados/ChamadosRepository.java
@@ -0,0 +1,6 @@
+package br.com.cadastrochamados.cadastrochamados.Chamados;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ChamadosRepository extends JpaRepository <ChamadosModel, Long>{
+}
