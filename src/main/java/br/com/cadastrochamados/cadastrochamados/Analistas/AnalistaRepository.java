5cc65571ec05a950302510a5245a89953457cfc0
Dan-dev1305 <danielgoncalves.paz@gmail.com>
2026-07-11 16:28:46 -0300

    Criacao do repositorio AnalistaModel

---
+++ src/main/java/br/com/cadastrochamados/cadastrochamados/Analistas/AnalistaRepository.java
@@ -0,0 +1,6 @@
+package br.com.cadastrochamados.cadastrochamados.Analistas;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AnalistaRepository extends JpaRepository<AnalistaModel, Long> {
+}
