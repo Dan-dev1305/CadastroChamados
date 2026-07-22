a19f539e05cf33a46be20344c49cc97f377b581c
unknown <Administrator@ROMERITO.AD>
2026-07-22 13:58:11 -0300

    feat: Criação das rotas do chamados controller com mock

--- src/main/java/br/com/cadastrochamados/cadastrochamados/Chamados/ChamadoController.java
+++ src/main/java/br/com/cadastrochamados/cadastrochamados/Chamados/ChamadoController.java
@@ -1,16 +1,34 @@
 package br.com.cadastrochamados.cadastrochamados.Chamados;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping
+@RequestMapping("chamados")
 public class ChamadoController {
 
     @GetMapping("/chamadoMsg")
     public String menssagemInicial(){
         return "Resposta chamado";
+    }
+
+    @GetMapping("/listar")
+    public String listarChamados(){
+        return "Chamados listados";
+    }
+
+    @PostMapping("/criar")
+    public String criarChamado(){
+        return "Chamado criado";
+    }
+
+    @PutMapping("/alterar")
+    public String alterarChamadoPorId(){
+        return "Chamado alterado";
+    }
+
+    @DeleteMapping("/deletar")
+    public String deletarChamadoPorId(){
+        return "Chamado deletado";
     }
 
 }
