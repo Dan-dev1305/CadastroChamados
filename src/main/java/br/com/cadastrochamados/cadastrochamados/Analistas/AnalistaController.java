194e47a34bbb3e19046ade90553101b72abc891d
unknown <Administrator@ROMERITO.AD>
2026-07-22 13:48:14 -0300

    feat: Atualização do analista controller com mock de dados

--- src/main/java/br/com/cadastrochamados/cadastrochamados/Analistas/AnalistaController.java
+++ src/main/java/br/com/cadastrochamados/cadastrochamados/Analistas/AnalistaController.java
@@ -1,8 +1,6 @@
 package br.com.cadastrochamados.cadastrochamados.Analistas;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping
@@ -11,5 +9,35 @@ public class AnalistaController {
     @GetMapping("/analistaMsg")
     public String menssagemInicial(){
         return "Resposta Analista";
+    }
+
+    // Adicionar Analista (Create)
+    @PostMapping ("/criar")
+    public String criarAnalista(){
+        return "Analista criado";
+    }
+
+    // Mostrar todos os analistas (REad)
+    @GetMapping ("/todos")
+    public String mostrarTodosAnalistas(){
+        return "Mostrar todos os analistas";
+    }
+
+    // Mostrar todos os analistas por ID (REad)
+    @GetMapping ("/todosId")
+    public String mostrarTodosAnalistasPorId(){
+        return "Mostrar todos os analistas";
+    }
+
+    //Alterar dados dos analistas (Update)
+    @PutMapping ("/alterarId")
+    public String alterarAnalistasPorId(){
+        return "Analista alterado";
+    }
+
+    //Deletar Analistas (Delete)
+    @DeleteMapping ("/deletarId")
+    public String deletarAnalistaPorId(){
+        return "Analista deletado";
     }
 }
