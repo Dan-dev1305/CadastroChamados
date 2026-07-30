a5e27f162f3480118dc0f96aef6a14f0f0769fda
unknown <danielgoncalves.paz@gmail.com>
2026-07-29 22:12:40 -0300

    Chore: Inclui o dto pela entidade e mapeia manualmente no mapper

--- src/main/java/br/com/cadastrochamados/cadastrochamados/Analistas/AnalistaDTO.java
+++ src/main/java/br/com/cadastrochamados/cadastrochamados/Analistas/AnalistaDTO.java
@@ -1,4 +1,24 @@
 package br.com.cadastrochamados.cadastrochamados.Analistas;
 
+import br.com.cadastrochamados.cadastrochamados.Chamados.ChamadosModel;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+
 public class AnalistaDTO {
+
+    private Long id;
+    private String nome;
+    private String email;
+    private String senioridade;
+    private String especialidade;
+    private ChamadosModel chamados;
+
+
+
+
 }
--- src/main/java/br/com/cadastrochamados/cadastrochamados/Analistas/AnalistaMapper.java
+++ src/main/java/br/com/cadastrochamados/cadastrochamados/Analistas/AnalistaMapper.java
@@ -1,4 +1,28 @@
 package br.com.cadastrochamados.cadastrochamados.Analistas;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class AnalistaMapper {
+
+    public AnalistaModel map(AnalistaDTO analistaDTO){
+        AnalistaModel analistaModel = new AnalistaModel();
+        analistaModel.setId(analistaDTO.getId());
+        analistaModel.setNome(analistaDTO.getNome());
+        analistaModel.setEmail(analistaDTO.getEmail());
+        analistaModel.setSenioridade(analistaDTO.getSenioridade());
+        analistaModel.setChamados(analistaDTO.getChamados());
+        return analistaModel;
+    }
+
+    public AnalistaDTO map(AnalistaModel analistaModel){
+        AnalistaDTO analistaDTO = new AnalistaDTO();
+        analistaDTO.setId(analistaModel.getId());
+        analistaDTO.setNome(analistaModel.getNome());
+        analistaDTO.setEmail(analistaModel.getEmail());
+        analistaDTO.setSenioridade(analistaModel.getSenioridade());
+        analistaDTO.setChamados(analistaModel.getChamados());
+        return analistaDTO;
+    }
+
 }
